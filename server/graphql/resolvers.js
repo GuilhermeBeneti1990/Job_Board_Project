22095ab7e810c4d4a3d2e0918569bb131f6219ab
GuilhermeBeneti1990 <guilhermebeneti@icloud.com>
2023-07-26 20:34:36 +0200

    add CRUD using graphql and removed fake data

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,9 +1,45 @@
-import { getJobs } from '../db/jobs.js'
-import {  getCompany } from '../db/companies.js'
+import { GraphQLError } from 'graphql';
+import { getJobs, getJob, getJobsByCompany, createJob, deleteJob, updateJob } from '../db/jobs.js'
+import { getCompany } from '../db/companies.js'
 
 export const resolvers = {
     Query: {
         jobs: async () => getJobs(),
+
+        job: async (_, { id }) => {
+            const job = getJob(id);
+
+            if(!job) {
+                throw notFoundError('Not found any job with id: ', id);
+            }
+
+            return job;
+        },
+
+        company: async (_, { id }) => {
+            const company = await getCompany(id);
+            
+            if(!company) {
+                throw notFoundError('Not found any company with id: ', id);
+            }
+
+            return company;
+        }
+    },
+
+    Mutation: {
+        createJob: (_, { input: { title, description } }) => {
+            const companyId = 'FcxJkALei';
+            return createJob({ companyId, title, description });
+        },
+        updateJob: (_, { input: { id, title, description }}) => {
+            return updateJob({ id, title, description });
+        },
+        deleteJob: (_, { id }) => deleteJob(id)
+    },
+
+    Company: {
+        jobs: (company) => getJobsByCompany(company.id)
     },
 
     Job: {
@@ -14,4 +50,11 @@ export const resolvers = {
             return job.createdAt;
         }
     }
+}
+
+
+function notFoundError(message) {
+    return new GraphQLError(message, {
+        extensions: { code: ' NOT_FOUND'}
+    });
 }
